88697457c1dafaac7e74b8107ee79cca413f00fe
SiniyaMC363 <siniyamujeeb972@gmail.com>
2026-05-06 11:55:13 +0530

    Cloud push S3 storage completed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -23,6 +23,10 @@ from sklearn.ensemble import (
 
 import mlflow
 
+import dagshub
+dagshub.init(repo_owner='SiniyaMC363',
+             repo_name='networksecurity', mlflow=True)
+
 
 class ModelTrainer:
     def __init__(self, model_trainer_config: ModelTrainerConfig,
@@ -114,6 +118,8 @@ class ModelTrainer:
             preprocessor=preprocessor, model=best_model)
         save_object(
             self.model_trainer_config.trained_model_file_path, obj=network_model)
+
+        save_object("final_model/model.pkl", best_model)
 
         # model trainer artifact
         model_trainer_artifact = ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
